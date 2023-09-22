b23a5545b815eef16a6a21ca8f337732872f416c
우지민 <0542say@naver.com>
2023-09-22 17:13:01 +0900

    dbutil

--- src/com/ssafy/util/DBUtil.java
+++ src/com/ssafy/util/DBUtil.java
@@ -32,30 +32,6 @@ public class DBUtil {
 		return DriverManager.getConnection(URL, DB_ID, DB_PWD);
 	}
 	
-//	public static void close(PreparedStatement pstmt, Connection conn) {
-//		try {
-//			if(pstmt != null)
-//				pstmt.close();
-//			if(conn != null)
-//				conn.close();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public static void close(ResultSet rs, PreparedStatement pstmt, Connection conn) {
-//		try {
-//			if(rs != null)
-//				rs.close();
-//			if(pstmt != null)
-//				pstmt.close();
-//			if(conn != null)
-//				conn.close();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
-	
 	public void close(AutoCloseable... autoCloseables) {
 		for(AutoCloseable ac : autoCloseables) {
 			if(ac != null) {
